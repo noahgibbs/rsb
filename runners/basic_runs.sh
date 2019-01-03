5786653383b5c688685c3b984694db93f0b261b5
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-01-03 10:09:56 -0800

    Use data directory for output in runner

--- runners/basic_runs.sh
+++ runners/basic_runs.sh
@@ -13,11 +13,11 @@ do
 
   # Rails: migrate as precommand, use widget_tracker dir
   cd widget_tracker
-  ../ab_bench.rb --url http://127.0.0.1:PORT/simple_bench/static -n 10000 -w 100 --server-command "rails server -p PORT" --server-pre-command "bundle exec rake db:migrate" --server-kill-match "rails server" -o rsb_rails_TIMESTAMP.json
+  ../ab_bench.rb --url http://127.0.0.1:PORT/simple_bench/static -n 10000 -w 100 --server-command "rails server -p PORT" --server-pre-command "bundle exec rake db:migrate" --server-kill-match "rails server" -o ../data/rsb_rails_TIMESTAMP.json
   cd ..
 
   # Rack: no precommand, use rack_hello_world dir
   cd rack_hello_world
-  ../ab_bench.rb --url http://127.0.0.1:PORT/simple_bench/static -n 10000 -w 100 --server-command "rackup -p PORT" --server-pre-command "echo Skip..." --server-kill-match "rackup" -o rsb_rack_TIMESTAMP.json
+  ../ab_bench.rb --url http://127.0.0.1:PORT/simple_bench/static -n 10000 -w 100 --server-command "rackup -p PORT" --server-pre-command "echo Skip..." --server-kill-match "rackup" -o ../data/rsb_rack_TIMESTAMP.json
   cd ..
 done
