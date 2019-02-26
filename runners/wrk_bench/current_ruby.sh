a00602e51a96e18ad1965c9c9b8839c8a1f99c44
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-02-26 06:52:02 -0800

    WIP: debug new runner script with new parameters

--- runners/wrk_bench/current_ruby.sh
+++ runners/wrk_bench/current_ruby.sh
@@ -1,22 +1,24 @@
 #!/bin/bash -l
 
-set -e
-set -x
+#set -e
+#set -x
 
-export RSB_RUBY_VERSION=${RSB_RUBY_VERSION:-`rvm current`}
+export RSB_RUBY_VERSION=${RSB_RUBY_VERSION:-`ruby -e 'puts RUBY_VERSION'`}
 export WRK_BENCH=${WRK_BENCH:-../wrk_bench.rb}
 export WRK=${WRK:-~/wrk/wrk}
 
 export BUNDLE_GEMFILE="Gemfile.$RSB_RUBY_VERSION"
-export RAILS_ENV=${RSB_RACK_ENV:production}
-export RACK_ENV=${RSB_RACK_ENV:production}
+export RAILS_ENV=${RSB_RACK_ENV:-production}
+export RACK_ENV=${RSB_RACK_ENV:-production}
+
+export BUNDLER_VERSION=${RSB_BUNDLER_VERSION:-}
 
 # Rails: migrate as precommand, use widget_tracker dir
 cd widget_tracker
-$WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 --server-command "bundle _1.17.3_ exec rails server -p PORT" --server-pre-command "bundle _1.17.3_ && bundle _1.17.3_ exec rake db:migrate" --server-kill-match "rails server" -o ../data/rsb_rails_TIMESTAMP.json
+$WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 --server-command "bundle $BUNDLER_VERSION exec rails server -p PORT" --server-pre-command "bundle $BUNDLER_VERSION && bundle $BUNDLER_VERSION exec rake db:migrate" --server-kill-match "rails server" -o ../data/rsb_rails_TIMESTAMP.json
 cd ..
 
 # Rack: no precommand, use rack_hello_world dir
 cd rack_hello_world
-$WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 --server-command "bundle _1.17.3_ && bundle _1.17.3_ exec rackup -p PORT" --server-pre-command "bundle _1.17.3_" --server-kill-match "rackup" -o ../data/rsb_rack_TIMESTAMP.json
+$WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 --server-command "bundle $BUNDLER_VERSION && bundle $BUNDLER_VERSION exec rackup -p PORT" --server-pre-command "bundle $BUNDLER_VERSION" --server-kill-match "rackup" -o ../data/rsb_rack_TIMESTAMP.json
 cd ..
