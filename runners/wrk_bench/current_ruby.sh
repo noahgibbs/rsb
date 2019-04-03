c6d6ca8ee2c890ed20942b7d43713f98ae559483
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-04-03 12:59:58 +0900

    WIP: update current_ruby runner

--- runners/wrk_bench/current_ruby.sh
+++ runners/wrk_bench/current_ruby.sh
@@ -11,14 +11,14 @@ export BUNDLE_GEMFILE="Gemfile.$RSB_RUBY_VERSION"
 export RAILS_ENV=${RSB_RACK_ENV:-production}
 export RACK_ENV=${RSB_RACK_ENV:-production}
 
-export BUNDLER_VERSION=${RSB_BUNDLER_VERSION:-_1.17.3_}
+export LOCAL_BUNDLER_VERSION=${RSB_BUNDLER_VERSION:-_1.17.3_}
 
 # Rails: migrate as precommand, use widget_tracker dir
 cd widget_tracker
-$WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 --server-command "bundle $BUNDLER_VERSION exec rails server -p PORT" --server-pre-command "bundle $BUNDLER_VERSION && bundle $BUNDLER_VERSION exec rake db:migrate" --server-kill-match "rails server" -o ../data/rsb_rails_TIMESTAMP.json
+$WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 --server-command "bundle $LOCAL_BUNDLER_VERSION exec rails server -p PORT" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION && bundle $LOCAL_BUNDLER_VERSION exec rake db:migrate" --server-kill-match "rails server" -o ../data/rsb_rails_TIMESTAMP.json
 cd ..
 
 # Rack: no precommand, use rack_hello_world dir
 cd rack_hello_world
-$WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 --server-command "bundle $BUNDLER_VERSION && bundle $BUNDLER_VERSION exec rackup -p PORT" --server-pre-command "bundle $BUNDLER_VERSION" --server-kill-match "rackup" -o ../data/rsb_rack_TIMESTAMP.json
+$WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 --server-command "bundle $LOCAL_BUNDLER_VERSION && bundle $LOCAL_BUNDLER_VERSION exec rackup -p PORT" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION" --server-kill-match "rackup" -o ../data/rsb_rack_TIMESTAMP.json
 cd ..
