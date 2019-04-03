ca472b2489710ad6f4400745bc5003b252340812
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-04-03 12:52:31 +0900

    Don't use BUNDLER_VERSION for my own env var, conflicts w/ Bundler

--- runners/wrk_bench/appserver_runs.sh
+++ runners/wrk_bench/appserver_runs.sh
@@ -12,7 +12,7 @@ export CONCURRENCY=${CONCURRENCY:-1}
 export RAILS_ENV=${RSB_RACK_ENV:-production}
 export RACK_ENV=${RSB_RACK_ENV:-production}
 
-export BUNDLER_VERSION=${RSB_BUNDLER_VERSION:-_1.17.3_}
+export LOCAL_BUNDLER_VERSION=${RSB_BUNDLER_VERSION:-_1.17.3_}
 
 
 # Server-Specific tuning
@@ -35,23 +35,23 @@ do
 
   # Rails: migrate as precommand, use widget_tracker dir
   cd widget_tracker
-  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $BUNDLER_VERSION exec rails server -p PORT" --server-pre-command "bundle $BUNDLER_VERSION && bundle $BUNDLER_VERSION exec rake db:migrate" --server-kill-match "rails server" -o ../data/rsb_rails_TIMESTAMP.json
+  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $LOCAL_BUNDLER_VERSION exec rails server -p PORT" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION && bundle $LOCAL_BUNDLER_VERSION exec rake db:migrate" --server-kill-match "rails server" -o ../data/rsb_rails_TIMESTAMP.json
   cd ..
 
   # Rack: no precommand, use rack_hello_world dir
   cd rack_hello_world
-  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $BUNDLER_VERSION exec rackup -p PORT" --server-pre-command "bundle $BUNDLER_VERSION" --server-kill-match "rackup" -o ../data/rsb_rack_TIMESTAMP.json
+  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $LOCAL_BUNDLER_VERSION exec rackup -p PORT" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION" --server-kill-match "rackup" -o ../data/rsb_rack_TIMESTAMP.json
   cd ..
 
   # Now do Puma - killing Rackup won't kill Puma properly
   export RSB_EXTRA_GEMFILES=Gemfile.puma
 
   cd widget_tracker
-  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $BUNDLER_VERSION exec rails server -p PORT" --server-pre-command "bundle $BUNDLER_VERSION && bundle $BUNDLER_VERSION exec rake db:migrate" --server-kill-match "puma" -o ../data/rsb_rails_TIMESTAMP.json
+  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $LOCAL_BUNDLER_VERSION exec rails server -p PORT" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION && bundle $LOCAL_BUNDLER_VERSION exec rake db:migrate" --server-kill-match "puma" -o ../data/rsb_rails_TIMESTAMP.json
   cd ..
 
   cd rack_hello_world
-  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $BUNDLER_VERSION exec rackup -p PORT" --server-pre-command "bundle $BUNDLER_VERSION" --server-kill-match "puma" -o ../data/rsb_rack_TIMESTAMP.json
+  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $LOCAL_BUNDLER_VERSION exec rackup -p PORT" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION" --server-kill-match "puma" -o ../data/rsb_rack_TIMESTAMP.json
   cd ..
 
 
@@ -59,22 +59,22 @@ do
   export RSB_EXTRA_GEMFILES=Gemfile.passenger
 
   cd widget_tracker
-  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $BUNDLER_VERSION exec passenger start -p PORT --log-level 2" --server-pre-command "bundle $BUNDLER_VERSION && bundle exec rake db:migrate" --server-kill-command "bundle $BUNDLER_VERSION exec passenger stop -p 4323" -o ../data/rsb_rails_TIMESTAMP.json
+  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $LOCAL_BUNDLER_VERSION exec passenger start -p PORT --log-level 2" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION && bundle exec rake db:migrate" --server-kill-command "bundle $LOCAL_BUNDLER_VERSION exec passenger stop -p 4323" -o ../data/rsb_rails_TIMESTAMP.json
   cd ..
 
   cd rack_hello_world
-  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $BUNDLER_VERSION exec passenger start -p PORT --log-level 2" --server-pre-command "bundle $BUNDLER_VERSION" --server-kill-command "bundle $BUNDLER_VERSION exec passenger stop -p 4323" -o ../data/rsb_rack_TIMESTAMP.json
+  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $LOCAL_BUNDLER_VERSION exec passenger start -p PORT --log-level 2" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION" --server-kill-command "bundle $LOCAL_BUNDLER_VERSION exec passenger stop -p 4323" -o ../data/rsb_rack_TIMESTAMP.json
   cd ..
 
   # Passenger-Tuned
   export RSB_EXTRA_GEMFILES=Gemfile.passenger-tuned
 
   cd widget_tracker
-  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $BUNDLER_VERSION exec passenger start -p PORT --log-level 2 --max-pool-size $PASSENGER_PROCESSES --min-instances $PASSENGER_PROCESSES --engine=builtin --passenger-pre-start" --server-pre-command "bundle $BUNDLER_VERSION && bundle exec rake db:migrate" --server-kill-command "bundle $BUNDLER_VERSION exec passenger stop -p 4323" -o ../data/rsb_rails_TIMESTAMP.json
+  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $LOCAL_BUNDLER_VERSION exec passenger start -p PORT --log-level 2 --max-pool-size $PASSENGER_PROCESSES --min-instances $PASSENGER_PROCESSES --engine=builtin --passenger-pre-start" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION && bundle exec rake db:migrate" --server-kill-command "bundle $LOCAL_BUNDLER_VERSION exec passenger stop -p 4323" -o ../data/rsb_rails_TIMESTAMP.json
   cd ..
 
   cd rack_hello_world
-  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $BUNDLER_VERSION exec passenger start -p PORT --log-level 2 --max-pool-size $PASSENGER_PROCESSES --min-instances $PASSENGER_PROCESSES --engine=builtin --passenger-pre-start" --server-pre-command "bundle $BUNDLER_VERSION" --server-kill-command "bundle $BUNDLER_VERSION exec passenger stop -p 4323" -o ../data/rsb_rack_TIMESTAMP.json
+  $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $LOCAL_BUNDLER_VERSION exec passenger start -p PORT --log-level 2 --max-pool-size $PASSENGER_PROCESSES --min-instances $PASSENGER_PROCESSES --engine=builtin --passenger-pre-start" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION" --server-kill-command "bundle $LOCAL_BUNDLER_VERSION exec passenger stop -p 4323" -o ../data/rsb_rack_TIMESTAMP.json
   cd ..
 
   # Passenger Enterprise ("passent")
@@ -83,11 +83,11 @@ do
   if [! -z "$RSB_PASSENT"]
   then
     cd widget_tracker
-    $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $BUNDLER_VERSION exec passenger start -p PORT --log-level 2 --max-pool-size $PASSENT_PROCESSES --min-instances $PASSENT_PROCESSES --passenger-concurrency-model thread --passenger-thread-count $PASSENT_THREADS --engine=builtin --passenger-pre-start" --server-pre-command "bundle $BUNDLER_VERSION && bundle exec rake db:migrate" --server-kill-command "bundle $BUNDLER_VERSION exec passenger stop -p 4323" -o ../data/rsb_rails_TIMESTAMP.json
+    $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $LOCAL_BUNDLER_VERSION exec passenger start -p PORT --log-level 2 --max-pool-size $PASSENT_PROCESSES --min-instances $PASSENT_PROCESSES --passenger-concurrency-model thread --passenger-thread-count $PASSENT_THREADS --engine=builtin --passenger-pre-start" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION && bundle exec rake db:migrate" --server-kill-command "bundle $LOCAL_BUNDLER_VERSION exec passenger stop -p 4323" -o ../data/rsb_rails_TIMESTAMP.json
     cd ..
 
     cd rack_hello_world
-    $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $BUNDLER_VERSION exec passenger start -p PORT --log-level 2 --max-pool-size $PASSENGER_PROCESSES --min-instances $PASSENGER_PROCESSES --engine=builtin --passenger-pre-start" --server-pre-command "bundle $BUNDLER_VERSION" --server-kill-command "bundle $BUNDLER_VERSION exec passenger stop -p 4323" -o ../data/rsb_rack_TIMESTAMP.json
+    $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $LOCAL_BUNDLER_VERSION exec passenger start -p PORT --log-level 2 --max-pool-size $PASSENGER_PROCESSES --min-instances $PASSENGER_PROCESSES --engine=builtin --passenger-pre-start" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION" --server-kill-command "bundle $LOCAL_BUNDLER_VERSION exec passenger stop -p 4323" -o ../data/rsb_rack_TIMESTAMP.json
     cd ..
   fi
 
@@ -96,11 +96,11 @@ do
     export RSB_EXTRA_GEMFILES="Gemfile.$RSB_APPSERVER"
 
     cd widget_tracker
-    $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $BUNDLER_VERSION exec rails server -p PORT" --server-pre-command "bundle $BUNDLER_VERSION && bundle $BUNDLER_VERSION exec rake db:migrate" --server-kill-match "rails server" -o ../data/rsb_rails_TIMESTAMP.json
+    $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $LOCAL_BUNDLER_VERSION exec rails server -p PORT" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION && bundle $LOCAL_BUNDLER_VERSION exec rake db:migrate" --server-kill-match "rails server" -o ../data/rsb_rails_TIMESTAMP.json
     cd ..
 
     cd rack_hello_world
-    $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $BUNDLER_VERSION exec rackup -p PORT" --server-pre-command "bundle $BUNDLER_VERSION" --server-kill-match "rackup" -o ../data/rsb_rack_TIMESTAMP.json
+    $WRK_BENCH --wrk-path $WRK --url http://127.0.0.1:PORT/simple_bench/static -n 180 -w 20 -c $CONCURRENCY --server-command "bundle $LOCAL_BUNDLER_VERSION exec rackup -p PORT" --server-pre-command "bundle $LOCAL_BUNDLER_VERSION" --server-kill-match "rackup" -o ../data/rsb_rack_TIMESTAMP.json
     cd ..
   done
 
