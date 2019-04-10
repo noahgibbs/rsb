1ca0f85e32b8374a30ca4f98c4bb655e6d7a7a38
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-04-10 13:25:40 +0900

    Check process success manually since Linux is somehow failing to return the correct value from system()

--- bench_lib.rb
+++ bench_lib.rb
@@ -91,6 +91,8 @@ module BenchLib
 
     def url_available?
       system("curl #{@url} &>/dev/null")
+      $?.success? # For some horrible reason, (only) on Linux, "system" is returning true on failure w/ output suppressed...
+      # Example for irb: result=system("curl http://127.0.0.1:4321/static &>/dev/null")
     end
 
     def ensure_url_available
