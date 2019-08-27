8d544c87521b845cb7f96c3ff1c3b01a9254fc6a
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-08-27 11:14:42 -0700

    Minor debugging on TCPServer-based tiny server

--- tiny_servers/tcp_server.rb
+++ tiny_servers/tcp_server.rb
@@ -1,10 +1,18 @@
-# tcp_server.rb
+#!/usr/bin/env ruby
 # From: https://blog.appsignal.com/2016/11/23/ruby-magic-building-a-30-line-http-server-in-ruby.html
 
 require 'socket'
-server = TCPServer.new 5678
+server = TCPServer.new 9090
+
+MESSAGE=<<MSG.freeze
+HTTP/1.1 200 OK
+Content-Type: text/html
+Content-Length: 3
+
+OK
+MSG
 
 while session = server.accept
-  session.puts "Hello world! The time is #{Time.now}"
+  session.puts MESSAGE
   session.close
 end
